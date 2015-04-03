2ed23bf282162596c88a8a13021b568ea6259f65
santoshsankar <santoshsankar@gmail.com>
2015-04-03 08:46:52 -0400

    added pin/users associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,3 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user #belongs_to association must use singular term. Using plural will most likely kick back an error
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326141619) do
+ActiveRecord::Schema.define(version: 20150403120551) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
